81a32a937564fe8a3c1a7a1fcc891b44ee5953e7
DevaKrishna07 <devakrishna1418@gmail.com>
2025-06-27 11:48:51 +0530

    connected to render

--- EcommerceFrontend/src/services/Api.ts
+++ EcommerceFrontend/src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = "https://ecommerceapp-1-yqy3.onrender.com//";
 
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
