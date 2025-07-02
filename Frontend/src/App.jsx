5905cb89bfb957789d4fff33c426d56f9ef3672d
DevaKrishna07 <devakrishna1418@gmail.com>
2025-07-02 22:21:24 +0530

    ..

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import './App.css'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
